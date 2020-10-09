180f645da1430fdbd0df7c97ff6dc3060d27c4a1
yg <kyk5165@naver.com>
2020-10-09 20:01:15 +0900

    Updates

---
+++ precache-manifest.8e5b6e1b1420736f79cc2c3f33d80298.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f27dd69770addac32c2be992aae1990",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "a3aa04a80ebf4feff633",
+    "url": "/movie_app_2020/static/css/main.404f5290.chunk.css"
+  },
+  {
+    "revision": "11748d6dc8da0fe94e79",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2020/static/js/2.8f3b569d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3aa04a80ebf4feff633",
+    "url": "/movie_app_2020/static/js/main.ee7ffb63.chunk.js"
+  },
+  {
+    "revision": "6636fcdddd453d60abc3",
+    "url": "/movie_app_2020/static/js/runtime-main.9a083715.js"
+  }
+]);
